01c4f32f4c2fbaace1733cd82438d46bb72a0303
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-08 21:18:23 +0530

    Sort Array By Dutch National flag Algorithm

---
+++ SortArray_DutchNationalFlag.java
@@ -0,0 +1,56 @@
+public class SortArray_DutchNationalFlag {
+    static void sortColors(int[] a) {
+        int low = 0, mid = 0, high = a.length - 1;
+
+        // while (mid <= high) {
+        //     switch (a[mid]) {
+        //         case 0:
+        //             // swap a[low] and a[mid]
+        //             int temp0 = a[low];
+        //             a[low] = a[mid];
+        //             a[mid] = temp0;
+        //             low++;
+        //             mid++;
+        //             break;
+        //         case 1:
+        //             mid++;
+        //             break;
+        //         case 2:
+        //             // swap a[mid] and a[high]
+        //             int temp2 = a[mid];
+        //             a[mid] = a[high];
+        //             a[high] = temp2;
+        //             high--;
+        //             break;
+        //     }
+        // }
+        while(mid <= high){
+        switch(a[mid]){
+            case 0://Swap a[mid] and a[low]
+            int temp0 = a[low];
+            a[low] = a[mid];
+            a[mid] = temp0;
+            low++;
+            mid++;
+            break;
+            case 1:
+            mid++;
+            break;
+            case 2://Swap a[mid] and a[high]
+            int temp2 = a[high];
+            a[high] = a[mid];
+            a[mid] = temp2;
+            high--;
+            break;
+        }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 0, 1, 2, 0, 1};
+        sortColors(a);
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
