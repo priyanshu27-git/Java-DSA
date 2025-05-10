f53abc170f339946fb48616f41e928d92f08ea4c
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-10 20:12:01 +0530

    corrected Move odd numbers at starting and even numbers to end

--- positivetoendnegativeinstart.java
+++ positivetoendnegativeinstart.java
@@ -1,25 +1,30 @@
+
 public class positivetoendnegativeinstart {
-     static int[] Movenumbers(int a[]) {
-        int arr[] = new int[a.length]; 
-        int left = 0,right = arr.length - 1;
-       for (int i = 0; i < a.length; i++){
-        if(left < right){
-        if(a[i] % 2 != 0){
-        arr[left] = a[i];
+
+    static int[] Movenumbers(int a[]) {
+        int arr[] = new int[a.length];
+        int left = 0, right = arr.length - 1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 != 0) {
+                arr[left] = a[i];
+                left++;
+            } else if (a[i] % 2 == 0) {
+                arr[right] = a[i];
+                right--;
+            }
         }
-        else if(a[i] % 2 == 0){
-        arr[right] = a[i];
-        }
+        return arr;
     }
-        left++;
-        right--;
-       }
-       return arr;
-    }
+
     public static void main(String[] args) {
-        int []a = {4,6,2,3,7,9};
+        int[] a = {4, 5, 2, 3, 6, 9};
+        System.out.println("Original Array ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        System.out.println("Reversed Array ");
         int arr[] = Movenumbers(a);
-        for (int i : arr) {
+        for (int i : arr) {//For each loop 
             System.out.println(i);
         }
     }
