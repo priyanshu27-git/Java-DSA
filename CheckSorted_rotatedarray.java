e424b7b2fd412caa870449dad1bb464c70020fe4
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-16 16:23:19 +0530

    Check array is sorted and rotated

---
+++ CheckSorted_rotatedarray.java
@@ -0,0 +1,26 @@
+
+public class CheckSorted_rotatedarray {
+
+    static void sorted_rotated(int a[]) {
+        int count = 0;
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i+1]) {
+                count++;
+            }
+        }
+        boolean islastbreakfirst = a[n-1] > a[0];
+        if (count == 0 && islastbreakfirst) {
+            System.out.println("Array is Sorted but not rotated"); 
+        }else if (count == 1 && !islastbreakfirst) {
+            System.out.println("Array is Sorted and Rotated"); 
+        }else {
+            System.out.println("Array is not Sorted and Rotated");
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = {2,1,3,4,5};
+        sorted_rotated(a);
+    }
+}
