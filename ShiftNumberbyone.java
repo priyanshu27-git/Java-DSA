9719fcc7a6ac4e9bb35bc3ceca5dd64f9ff648aa
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-16 16:24:00 +0530

    updated

--- ShiftNumberbyone.java
+++ ShiftNumberbyone.java
@@ -1,17 +1,25 @@
+
 public class ShiftNumberbyone {
-     static int[] shiftNumber(int a[]) {
+
+    static int[] shiftNumber(int a[]) {
         int last = a[a.length - 1];
         for (int i = a.length - 1; i > 0; i--) {
-            a[i] = a[i-1];
+            a[i] = a[i - 1];
         }
         a[0] = last;
         return a;
     }
+
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,5};
+        int a[] = {1, 2, 3, 4, 5};
+        System.out.println("Original Array ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("\nUpdated Array ");
         int arr[] = shiftNumber(a);
-        for (int i : arr) {
-            System.out.println(i);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(a[i] + " ");
         }
     }
 }
