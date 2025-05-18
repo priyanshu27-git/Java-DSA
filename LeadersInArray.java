27e370166a8c9dddcaa8c5564313466dc6a4ca64
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-18 14:19:55 +0530

    Leaders in an array

---
+++ LeadersInArray.java
@@ -0,0 +1,26 @@
+
+public class LeadersInArray {
+
+    static void Leaders(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            boolean checkleader = false;
+            for (int j = i; j < a.length - 1; j++) {
+                if (a[i] < a[j + 1]) {
+                    break;
+                }
+                if (a[i] >= a[j + 1] && j == a.length - 2) {
+                    checkleader = true;
+                }
+            }
+            if (checkleader || i == a.length - 1) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5, 2};
+        System.out.println("Leaders in an Array ");
+        Leaders(a);
+    }
+}
