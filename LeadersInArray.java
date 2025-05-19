d26b85e57541667a6c7baf37670e19e9aea119df
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-19 16:21:13 +0530

    Updated Leaders in an array

--- LeadersInArray.java
+++ LeadersInArray.java
@@ -19,7 +19,7 @@ public class LeadersInArray {
     }
 
     public static void main(String[] args) {
-        int a[] = {1, 2, 3, 4, 5, 2};
+        int a[] = {16, 17, 3, 4, 5, 2};
         System.out.println("Leaders in an Array ");
         Leaders(a);
     }
