6d1190246dd19dcf762c46610a76e839c9919960
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-24 22:59:36 +0530

    50 day Java Dsa q1

---
+++ Array/MaximumAndMinimum.java
@@ -0,0 +1,23 @@
+public class MaximumAndMinimum {
+    public static void MaxAndMin(int arr[]) { 
+        int max = 1;
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            else if (arr[i] < min){
+             min = arr[i];
+            }
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+        System.out.println("Maximum element = " +max);
+        System.out.println("Minimum element = " +min);
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,8,9,2,7};
+        MaxAndMin(arr);
+    }
+}
