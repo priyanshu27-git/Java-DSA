b9d8b3bbc35bf49349274531b418db5625580983
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-26 22:00:21 +0530

    Trapping Rain Water

---
+++ Array/TrappingRainWater.java
@@ -0,0 +1,26 @@
+
+public class TrappingRainWater {
+
+    static int TrapWater(int a[]) {
+        int result = 0;
+        for (int i = 1; i < a.length - 1; i++) {
+            int left = a[i];
+            for (int j = 0; j < i; j++) {
+                left = left > a[j] ? left : a[j];//left max
+            }
+            int right = a[i];
+            for (int j = i + 1; j < a.length; j++) {
+                right = right > a[j] ? right : a[j];//right max
+            }
+
+            result += ((left < right ? left : right) - a[i]); // update maximum water
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int ans = TrapWater(a);
+        System.out.println(ans);
+    }
+}
