ed786dce862bba76563d1b46be6ed8e97d1da9be
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-05-26 23:30:11 +0530

    updated

--- Array/TrappingRainWater.java
+++ Array/TrappingRainWater.java
@@ -19,7 +19,7 @@ public class TrappingRainWater {
     }
 
     public static void main(String[] args) {
-        int a[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int a[] = {2, 1, 5, 3, 1, 0, 4};
         int ans = TrapWater(a);
         System.out.println(ans);
     }
