ccf3a60d307a1c56556c9fa42315adec9a3b826d
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-06-02 21:21:23 +0530

    updated

--- CheckAnagramsString.java
+++ CheckAnagramsString.java
@@ -1,18 +1,11 @@
-
 import java.util.Arrays;
-
 public class CheckAnagramsString {
 
     static boolean Anagrams(String str, String str1) {
-        int n1 = str.length();
-        int n2 = str1.length();
-        char[] ch1 = new char[n1];
-        char[] ch2 = new char[n2];
-        for (int i = 0; i < ch1.length; i++) {
-            ch1[i] = str.charAt(i);
-        }
-        for (int i = 0; i < ch2.length; i++) {
-            ch2[i] = str1.charAt(i);
+        char[] ch1 = str.toCharArray();
+        char[] ch2 = str1.toCharArray();
+        if(ch1.length != ch2.length){
+        return false;
         }
         Arrays.sort(ch1);
         Arrays.sort(ch2);
