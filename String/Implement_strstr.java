031e6009c65d3acdf3bcc90c2caa4a46c90e9639
priyanshu27-git <sahupriyanshu888a@gmail.com>
2025-06-06 13:32:18 +0530

    Implement strstr function without using in-built function

---
+++ String/Implement_strstr.java
@@ -0,0 +1,33 @@
+
+public class Implement_strstr {
+
+    public static int strstr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            boolean found = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "priyanshu";
+        String needle = "anshu";
+        int ans = strstr(haystack, needle);
+        System.out.println(ans);
+    }
+}
